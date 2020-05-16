ad8223b924d45d4305a9f972fe3a107bb84df42d
Nabi Zakaria <nabi.zakaria.developer@gmail.com>
2020-05-16 15:27:21 +0100

    Dependencies configuration + Login Interface design

--- app/src/main/java/com/pfe/enginapp/ui/Login.java
+++ app/src/main/java/com/pfe/enginapp/ui/Login.java
@@ -1,14 +1,16 @@
-package com.pfe.enginapp;
+package com.pfe.enginapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.pfe.enginapp.R;
+
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/pfe/enginapp/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pfe.enginapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pfe.enginapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
